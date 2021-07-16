4047945c9800d7db1ef919d0944d776ecce45c2c
E-Ozdemir <emreclarusway@gmail.com>
2021-07-16 23:57:45 +0200

    first commit

---
+++ to_do.js
@@ -0,0 +1,23 @@
+
+
+// console.log(document.querySelector(".add-button"));
+
+// document.querySelector(".add-button").addEventListener("click",()=>{
+    
+//     const inputContent = document.querySelector(".task-adder-input").innerHTML;
+//     console.log("inputContent:",inputContent);
+//     const node = document.createElement("li");
+//     console.log(node.appendChild(inputContent));
+//     node.appendChild(inputContent);
+//     document.querySelector(".tasks").appendChild(node);
+// });
+
+function addFunction(){
+    const inputContent = document.querySelector(".task-adder-input").value;
+    console.log("inputContent:",inputContent);
+    const node = document.createElement("li");
+    node.innerText= inputContent;
+    console.log('node:',node);
+    document.querySelector(".tasks").appendChild(node);
+}
+
