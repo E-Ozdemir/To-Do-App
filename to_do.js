2bbd473b4165ad80915e9448cd6b8f4d82fd8d22
E-Ozdemir <emreclarusway@gmail.com>
2021-07-21 23:50:13 +0200

    some new features

--- to_do.js
+++ to_do.js
@@ -1,30 +1,27 @@
-
-
-// console.log(document.querySelector(".add-button"));
-
-// document.querySelector(".add-button").addEventListener("click",()=>{
-    
-//     const inputContent = document.querySelector(".task-adder-input").innerHTML;
-//     console.log("inputContent:",inputContent);
-//     const node = document.createElement("li");
-//     console.log(node.appendChild(inputContent));
-//     node.appendChild(inputContent);
-//     document.querySelector(".tasks").appendChild(node);
-// });
-
-function addFunction(){
-    const inputContent = document.querySelector(".task-adder-input").value;
-    if(inputContent===''){
-        alert('You can not add an empty input! ')
-    }
-    else{
-
-        console.log("inputContent:",inputContent);
-        const node = document.createElement("li");
-        node.innerText= inputContent;
-        // console.log('node:',node);
-        document.querySelector(".tasks").appendChild(node);
-        document.querySelector(".task-adder-input").value ='';
-    }
+function addFunction() {
+  const inputContent = document.querySelector(".task-adder-input").value;
+  if (inputContent === "") {
+    alert("You can not add an empty input! ");
+  } else {
+    const node = document.createElement("LI");
+    node.className = "checked";
+    node.innerText = inputContent;
+    console.log("node:", node);
+    document.querySelector(".tasks").appendChild(node);
+    document.querySelector(".task-adder-input").value = "";
+  }
 }
 
+// Add a "checked" symbol when clicking on a list item
+
+var list = document.querySelector(".tasks");
+
+list.addEventListener(
+  "click",
+  function (ev) {
+    if (ev.target.tagName === "LI") {
+      ev.target.classList.toggle("checked");
+    }
+  },
+  false
+);
